840918145ab64e324906b30a3bca5722231e3b2a
thanhtunguet <thanhtung.uet@gmail.com>
2024-08-17 12:03:14 +0700

    Fix generate html

--- src/components/GritCenterRenderer.tsx
+++ src/components/GritCenterRenderer.tsx
@@ -9,6 +9,7 @@ import "ace-builds/src-noconflict/theme-github";
 import {
   GritCenterAnswer,
   GritCenterExamData,
+  GritCenterPart,
   GritCenterSimpleQuestion,
 } from "../models/grit-center";
 
@@ -24,119 +25,42 @@ const generateHTMLContent = (data: GritCenterExamData) => {
     </head>
     <body>
         <h1>${data.name}</h1>
-        <h2>Part 1: PHONETICS (5 points)</h2>
-
-        <h3>Section 1: Choose the word whose underlined part is pronounced differently from that of the others in the same line and write A, B, C or D on your answer sheet.</h3>
-        ${data.part[0].section[0].simple_questions
+        ${data.part
           .map(
-            (question: GritCenterSimpleQuestion, index: number) => `
-        <p>${index + 1}. ${question.answers
+            (part: GritCenterPart, partIndex: number) => `
+        <h2>Part ${partIndex + 1}: ${part.content.replace(
+              /<\/?[^>]+(>|$)/g,
+              ""
+            )}</h2>
+
+        ${part.simple_questions
+          .map(
+            (question: GritCenterSimpleQuestion, questionIndex: number) => `
+        <p>${questionIndex + 1}. ${question.content.replace(
+              /<\/?[^>]+(>|$)/g,
+              ""
+            )}</p>
+        <ul>
+            ${question.answers
               .map(
-                (answer: GritCenterAnswer, i: number) => `
-           <strong>${String.fromCharCode(65 + i)}.</strong> ${answer.content}
-        `
+                (answer: GritCenterAnswer, answerIndex: number) => `
+            <li><strong>${String.fromCharCode(65 + answerIndex)}.</strong> ${
+                  answer.content
+                }</li>
+            `
               )
-              .join(", ")}</p>`
-          )
-          .join("")}
-
-        <h3>Section 2: Choose the word whose stress pattern is different from that of the others in the same line and write A, B, C or D on your answer sheet.</h3>
-        ${data.part[0].section[1].simple_questions
-          .map(
-            (question: GritCenterSimpleQuestion, index: number) => `
-        <p>${index + 4}. ${question.answers
-              .map(
-                (answer: GritCenterAnswer, i: number) => `
-           <strong>${String.fromCharCode(65 + i)}.</strong> ${answer.content}
+              .join("")}
+        </ul>
+        <p><em>Explanation:</em> ${question.explain.replace(
+          /<\/?[^>]+(>|$)/g,
+          ""
+        )}</p>
         `
-              )
-              .join(", ")}</p>`
           )
           .join("")}
-
-        <h2>Part 2: GRAMMAR AND VOCABULARY (30 points)</h2>
-
-        <h3>Section 1: Supply the correct forms of the verbs.</h3>
-        ${data.part[1].section[0].simple_questions
-          .map(
-            (_: GritCenterSimpleQuestion, index: number) => `
-        <p>${index + 6}. ________________________</p>`
+        `
           )
           .join("")}
-
-        <h3>Section 2: Supply the correct form of the words in brackets. Write the answers on your answer sheet.</h3>
-        ${data.part[1].section[1].simple_questions
-          .map(
-            (_: GritCenterSimpleQuestion, index: number) => `
-        <p>${index + 13}. ________________________</p>`
-          )
-          .join("")}
-
-        <h3>Section 3: There are six mistakes in the passage. Find out and correct them. Write the answers on your answer sheet. The first one has been done for you as an example.</h3>
-        <p>Example: Line 1: is -> are</p>
-        ${data.part[1].section[2].simple_questions
-          .map(
-            (_, index: number) => `
-        <p>${index + 21}. ________________________</p>`
-          )
-          .join("")}
-
-        <h2>Part 3: READING (30 points)</h2>
-
-        <h3>Section 1: Read the following passage and choose the most suitable phrase from A-F to fill in each gap. There is one extra phrase that you don't need to use. Write the answers on your answer sheet.</h3>
-        <p><strong>Passage:</strong></p>
-        <p>${data.part[2].section[0].simple_questions
-          .map(
-            (_, index: number) => `
-        ${index + 36}. ________________________</p>`
-          )
-          .join("")}</p>
-
-        <h3>Section 2: Read the passage and choose the best answer to each question that follows. Write A, B, C, or D on your answer sheet.</h3>
-        <p><strong>Passage:</strong></p>
-        ${data.part[2].section[1].simple_questions
-          .map(
-            (_, index: number) => `
-        <p>${index + 41}. ________________________</p>`
-          )
-          .join("")}
-
-        <h3>Section 3: Read the passage and choose the most suitable word from the ones given below to fill in each gap. Write A, B, C, or D on your answer sheet.</h3>
-        <p><strong>Passage:</strong></p>
-        ${data.part[2].section[2].simple_questions
-          .map(
-            (_, index: number) => `
-        <p>${index + 46}. ________________________</p>`
-          )
-          .join("")}
-
-        <h3>Section 4: Read the following passage then fill in each gap with ONE suitable word. Write the answers on your answer sheet.</h3>
-        <p><strong>Passage:</strong></p>
-        ${data.part[2].section[3].simple_questions
-          .map(
-            (_, index: number) => `
-        <p>${index + 56}. ________________________</p>`
-          )
-          .join("")}
-
-        <h2>Part 4: WRITING (20 points)</h2>
-
-        <h3>Section 1: Rewrite each of the following sentences beginning as shown, so that the meaning stays the same.</h3>
-        ${data.part[3].section[0].simple_questions
-          .map(
-            (_, index: number) => `
-        <p>${index + 66}. ________________________</p>`
-          )
-          .join("")}
-
-        <h3>Section 2: Rewrite each of the following sentences using the given words so that it keeps the same meaning. Do not change the form of the words given.</h3>
-        ${data.part[3].section[1].simple_questions
-          .map(
-            (_, index: number) => `
-        <p>${index + 71}. ________________________</p>`
-          )
-          .join("")}
-
     </body>
     </html>
     `;
